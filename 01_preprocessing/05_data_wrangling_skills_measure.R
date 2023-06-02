75031f34bf31430a18efb7ce8a6d824969487034
MigMunozHa <miguel.munoz@nottingham.ac.uk>
2023-06-02 12:39:06 -0400

    Update 05_data_wrangling_skills_measure.R
    Code updated

--- 01_preprocessing/05_data_wrangling_skills_measure.R
+++ 01_preprocessing/05_data_wrangling_skills_measure.R
@@ -13,7 +13,7 @@ attr(LFS_clean$degree, "labels")
 table(LFS_clean$higher)
 attr(LFS_clean$higher, "labels")
 
-###Reviewing skills levels
+###Reviewing skills variables
 
 table(LFS_clean$quali, LFS_clean$degree)
 table(LFS_clean$quali, LFS_clean$higher)
@@ -28,13 +28,13 @@ table(LFS_clean$higher, LFS_clean$degree)
 
 LFS_clean <- LFS_clean %>% mutate(education = case_when(
   #Doctorate
-  higher == 1 & degree %in% 1:5 & quali %in% 1:7 & quali2 %in% 0:6 ~ 1,
+  higher == 1 & degree %in% 1:6 & quali %in% 1:7 & quali2 %in% 0:6 ~ 1,
   #Masters
-  higher == 2 & degree %in% 1:5 & quali %in% 1:7 & quali2 %in% 0:6 ~ 2,
+  higher == 2 & degree %in% 1:6 & quali %in% 1:7 & quali2 %in% 0:6 ~ 2,
   #PG Diploma
-  higher %in% 3:4 & degree %in% 1:5 & quali %in% 1:7 & quali2 %in% 0:6 ~ 3,
+  higher %in% 3:4 & degree %in% 1:6 & quali %in% 1:7 & quali2 %in% 0:6 ~ 3,
   #DK HE
-  higher == 5 & degree %in% 1:5 & quali %in% 1:7 & quali2 %in% 0:6 ~ 4,
+  higher == 5 & degree %in% 1:6 & quali %in% 1:7 & quali2 %in% 0:6 ~ 4,
   is.na(higher) & degree == 1 & quali %in% 1:7 & quali2 %in% 0:6 ~ 4,
   #First degree
   is.na(higher) & degree == 2 & quali %in% 1:7 & quali2 %in% 0:6 ~ 5,
@@ -75,37 +75,36 @@ LFS_clean$education <- set_labels(LFS_clean$education, labels = c("Doctorate","M
                                                                    "Higher education", "GCE, A-level or equivalent","GCSE grades A*-C or equivalent","Other qualification","No qualification","Don't know qualification")) 
 
 
-###Clustering skills and occupations
+###Clustering skills and occupations through normative approach 
 
 #Transforming skills into ISCED-97
 
 table(LFS_clean$education)
 attr(LFS_clean$education, "labels")
 
-LFS_clean <- LFS_clean %>% mutate(ISCED = case_when(education == 1 ~ 8,
-                                                    education %in% 2:4 ~ 7,
-                                                    education == 5 ~ 6,
-                                                    education %in% 6:9 ~ 5,
-                                                    education %in% 10:11 ~ 3,
+LFS_clean <- LFS_clean %>% mutate(ISCED = case_when(education == 1 ~ 6,
+                                                    education %in% 2:9 ~ 5,
+                                                    education == 10 ~ 4,
+                                                    education == 11 ~ 3,
                                                     education %in% 12:13 ~ 2,
                                                     education == 15 ~ 2,
                                                     education == 14 ~ 0,
                                                     is.na(education) ~ NA
 ))         
 
-#Clustering ISCED-97 into skill levels
+#Clustering ISCED-97 into levels
 
 table(LFS_clean$ISCED)
 
 LFS_clean <- LFS_clean %>% mutate(ISCED_cat = case_when(
-  ISCED %in% 6:8 ~ 3,
-  ISCED %in% 3:5 ~ 2,
-  ISCED %in% 0:2 ~ 1,
+  ISCED %in% 5:6 ~ 1,
+  ISCED %in% 3:4 ~ 2,
+  ISCED %in% 0:2 ~ 3,
   is.na(ISCED)  ~ NA
 ))
 
 LFS_clean$ISCED_cat <- set_labels(LFS_clean$ISCED_cat, 
-                             labels = c("Low-skilled","Intermediate","High-skilled"))
+                             labels = c("High-skilled","Intermediate","Low-skilled"))
 
 table(LFS_clean$ISCED_cat)
 attr(LFS_clean$ISCED_cat, "labels")
@@ -118,13 +117,13 @@ table(LFS_clean$occu1,LFS_clean$occu2)
 
 LFS_clean <- LFS_clean %>% mutate(ISCO = case_when(
   occu1 %in% 1:3 | occu2 %in% 1:3 ~ 1,
-  occu1 %in% 4:8 | occu2 %in% 4:8~ 2,
+  occu1 %in% 4:8 | occu2 %in% 4:8 ~ 2,
   occu1 == 9 | occu2 == 9 ~ 3,
   is.na(occu1) & is.na(occu2) ~ NA
 ))
 
 LFS_clean$ISCO <- set_labels(LFS_clean$ISCO, 
-                                 labels = c("Low-skilled","Intermediate","High-skilled"))
+                                 labels = c("High-skilled","Intermediate","Low-skilled"))
 
 table(LFS_clean$ISCO)
 attr(LFS_clean$ISCO, "labels")
@@ -143,6 +142,7 @@ LFS_clean <- LFS_clean %>% mutate(sk_mm = case_when(
 LFS_clean$sk_mm <- set_labels(LFS_clean$sk_mm, 
                              labels = c("Overeducated","Matched","Undereducated"))
 
+table(LFS_clean$ISCED_cat, LFS_clean$ISCO)
 table(LFS_clean$sk_mm)
 attr(LFS_clean$sk_mm, "labels")
 
@@ -153,5 +153,3 @@ glimpse(LFS_clean)
 save(LFS_clean, file = "./data/LFS_clean.rda")
 
 rm(list = ls()) # clean the global environment
-
-
