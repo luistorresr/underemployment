7b6051068af9039e9d5555bc5ddee4414594d2e3
Luis D. Torres <luistorresr@live.com>
2024-02-11 10:49:09 +0000

    Delete from github

--- 01_preprocessing/2_for_report_two_lfs/mteams_links.R
+++
@@ -1,9 +0,0 @@
-
-setwd("C:/Users/luist/The University of Nottingham/Underemployment - General/raw_datasets")
-
-save(LFS_clean, file = "./LFS_clean/LFS_clean.rda")
-
-
-##### Back to the working directory back to where you created the R-Studio project
-
-setwd("C:/Users/luist/OneDrive - The University of Nottingham/NUBS UK/4 - Research/Projects/ESRC - Underemployment/Analysis/underemployment")
